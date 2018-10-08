bc36f478b8e9b0e86e11b9a2806b4713f8d6aa34
Pawel Gerr <pawel.gerr@thinktecture.com>
2018-10-08 08:36:51 +0200

    Updated example to make it clear that structured logging is not possible with current compiler (https://github.com/dotnet/roslyn/issues/142).

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/ys8hswkylkqbf734?svg=true)](https://ci.appveyor.com/project/PawelGerr/thinktecture-extensions-logging)
 
+Adds string interpolation support to [ILogger](https://www.nuget.org/packages/Microsoft.Extensions.Logging.Abstractions/).
--- examples/Thinktecture.Extensions.Logging.Abstractions.Example/Program.cs
+++ examples/Thinktecture.Extensions.Logging.Abstractions.Example/Program.cs
@@ -1,27 +1,47 @@
 using System;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace Thinktecture.Extensions.Logging.Abstractions.Example
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var serilog = new LoggerConfiguration()
-				.WriteTo.Console()
-				.CreateLogger();
-			var logger = new LoggerFactory()
-				.AddSerilog(serilog)
-				.CreateLogger<Program>();
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var serilog = new LoggerConfiguration()
+                       .WriteTo.Console()
+                       .WriteTo.Sink(new MySink())
+                       .CreateLogger();
+         var logger = new LoggerFactory()
+                      .AddSerilog(serilog)
+                      .CreateLogger<Program>();
 
-			var a = "text";
-			var b = 42;
-			logger.LogInformation((string)$"Using standard extension method: a = {a}, b = {b}");
-			logger.LogInformation($"Using new extension method: a = {a}, b = {b}");
+         var a = "text";
+         var b = 42;
+         logger.LogInformation((string)$"Using standard extension method: a = {a}, b = {b}");
+         logger.LogInformation($"Using new extension method: a = {a}, b = {b}");
 
-			Console.WriteLine("Press ENTER to exit.");
-			Console.ReadLine();
-		}
-	}
+         Console.WriteLine("Press ENTER to exit.");
+         Console.ReadLine();
+      }
+   }
+
+   internal class MySink : ILogEventSink
+   {
+      public void Emit(LogEvent logEvent)
+      {
+         Console.WriteLine();
+         Console.WriteLine($"Template: {logEvent.MessageTemplate.Text}");
+         Console.WriteLine("Properties:");
+
+         foreach (var kvp in logEvent.Properties)
+         {
+            // Issue: formattable string does not capture the names of the placeholders
+            // https://github.com/dotnet/roslyn/issues/142
+            Console.WriteLine($"\t{kvp.Key} = {kvp.Value}");
+         }
+      }
+   }
 }
